410487b7b8105f4497aa1fc0fbf5bfa1afb01af1
CJ Roberts <cjroberts2013@gmail.com>
2020-04-23 00:09:53 -0500

    created a second test to test if state is equal to an empty list

--- project-1/src/components/App.test.js
+++ project-1/src/components/App.test.js
@@ -10,5 +10,12 @@ const app = shallow(<App />);
 
 it('renders correctly', () => {
     expect(toJson(app)).toMatchSnapshot();
-})
+});
 
+it('initializes the `state` with an empty list of gifts', () => {
+    expect(app.state().gifts).toEqual([])
+});
+
+it('adds a new gift to `state` when clicking the `add gift` button', () => {
+
+});
