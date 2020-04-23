f8bfdf04ffb6ecf10a6b099f3be78dfd4abba46f
CJ Roberts <cjroberts2013@gmail.com>
2020-04-23 07:34:25 -0500

    Created and passed test to check that a add gift button updates the state

--- project-1/src/components/App.test.js
+++ project-1/src/components/App.test.js
@@ -17,5 +17,7 @@ it('initializes the `state` with an empty list of gifts', () => {
 });
 
 it('adds a new gift to `state` when clicking the `add gift` button', () => {
+    app.find('.btn-add').simulate('click');
 
+    expect(app.state().gifts).toEqual([{ id: 1 }])
 });
